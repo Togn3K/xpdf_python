1a8e6826eb0c3e0d228036583c33915c720b217c
root <root@django-server.europe-west4-a.c.awhy-254209.internal>
2019-10-01 19:33:35 +0000

    fixed linux install script (again)Â

--- xpdf_python/install_xpdf/linux_install.sh
+++ xpdf_python/install_xpdf/linux_install.sh
@@ -2,9 +2,9 @@
 
 echo "INSTALLING XPDF"
 cd /tmp/
-wget https://xpdfreader-dl.s3.amazonaws.com/xpdf-tools-linux-4.01.01.tar.gz
-tar -xvzf xpdf-tools-linux-4.01.01.tar.gz
-cp xpdf-tools-linux-4.01.01/bin64/* /usr/local/bin
-cp xpdf-tools-linux-4.01.01/doc/sample-xpdfrc /usr/local/etc/xpdfrc
+wget https://xpdfreader-dl.s3.amazonaws.com/xpdf-tools-linux-4.02.tar.gz
+tar -xvzf xpdf-tools-linux-4.02.tar.gz
+cp xpdf-tools-linux-4.02/bin64/* /usr/local/bin
+cp xpdf-tools-linux-4.02/doc/sample-xpdfrc /usr/local/etc/xpdfrc
 
 
